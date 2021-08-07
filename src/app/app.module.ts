8ddfb6bb3e2377f297dc3e8f7b62e2da93c944a4
dmitry voronov <VoronovDN@sibintek.ru>
2021-08-07 16:57:08 +0300

    import the store and effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { appEffects, appReducers } from './core/store';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot(appEffects),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     HttpClientModule,
   ],
