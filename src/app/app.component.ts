0fb661f359f93a387a4bda0e3d56d682178275f8
dmitry voronov <VoronovDN@sibintek.ru>
2021-08-08 22:11:44 +0300

    add some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: {
+    'class': 'app-root'
+  },
 })
 export class AppComponent {
   title = 'github-search';
