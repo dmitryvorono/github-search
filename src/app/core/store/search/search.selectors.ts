c165b1cf72d51843eae897ba8673fae0f86f8d77
dmitry voronov <VoronovDN@sibintek.ru>
2021-08-08 18:18:16 +0300

    pretty

--- src/app/core/store/search/search.selectors.ts
+++ src/app/core/store/search/search.selectors.ts
@@ -1,8 +1,8 @@
 import { SearchState } from './search.state';
-import { createSelector } from "@ngrx/store";
-import { AppState } from "..";
+import { createSelector } from '@ngrx/store';
+import { AppState } from '..';
 
 export const selectRepositories = createSelector(
   (state: AppState) => state.search,
-  (state: SearchState) => state.result,
+  (state: SearchState) => state.result
 );
