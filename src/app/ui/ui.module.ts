76e66b6fcba43adba2f5e80081aad9926c4f5a70
dmitry voronov <VoronovDN@sibintek.ru>
2021-08-08 19:20:35 +0300

    add loading c and d

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,3 +1,4 @@
+import { LoadingDirective } from './directives/loading/loading.directive';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/search/search.component';
@@ -5,10 +6,29 @@ import { MatInputModule } from '@angular/material/input';
 import { CardComponent } from './components/card/card.component';
 import { MatCardModule } from '@angular/material/card';
 import { ListOfCardsComponent } from './components/list-of-cards/list-of-cards.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  declarations: [SearchComponent, CardComponent, ListOfCardsComponent],
-  imports: [CommonModule, MatInputModule, MatCardModule],
-  exports: [SearchComponent, CardComponent, ListOfCardsComponent],
+  declarations: [
+    SearchComponent,
+    CardComponent,
+    ListOfCardsComponent,
+    LoadingComponent,
+    LoadingDirective,
+  ],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+  ],
+  exports: [
+    SearchComponent,
+    CardComponent,
+    ListOfCardsComponent,
+    LoadingDirective,
+    LoadingComponent,
+  ],
 })
 export class UiModule {}
